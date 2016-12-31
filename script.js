c000d8281922e4f4e8d2a0c0169cfd265743aa38
Francisco Salgado <salgfrancisco@gmail.com>
2016-12-31 15:52:12 +0000

    fixed saved as image

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
+var CANVAS_HEIGHT = 480;
+var CANVAS_WIDTH = 640;
+
+
 document.addEventListener("DOMContentLoaded", function(event) {
-    var b = document.getElementById('getval')
-    b.addEventListener('change', readURL, true);
+    var generateButton = document.getElementById('generateButton')
+    generateButton.addEventListener('click', generate, true);
 });
 
 
-function readURL(){
-    var file = document.getElementById("getval").files[0];
+function generate(){
+    var file = document.getElementById("getFile").files[0];
     var reader = new FileReader();
     reader.onloadend = function(){
         var canvas = document.getElementById("mycanvas");
+        canvas.width = CANVAS_WIDTH;
+        canvas.height = CANVAS_HEIGHT;
         var context = canvas.getContext("2d");
-
 
         var imageObj = new Image();
         imageObj.onload = function(){
-            console.log("width: "+imageObj.width + " height: "+imageObj.height);
             var ratio = imageObj.width/imageObj.height;
             var newWidth = canvas.width;
             var newHeight = newWidth / ratio;
@@ -22,6 +26,9 @@ function readURL(){
                 newHeight = canvas.height;
                 newWidth = newHeight * ratio;
             }
+
+            canvas.width = newWidth;
+            canvas.height = newHeight;
 
             context.drawImage(imageObj, 0, 0, newWidth, newHeight);
 
@@ -35,12 +42,9 @@ function readURL(){
             text = "I " + whatido + " in #mycalvins"
 
             //Fit text in canvas
-            console.log("text width: " + context.measureText(text).width + " 0.9 image width: " + newWidth);
-
             while (context.measureText(text).width > 0.9*newWidth ) {
                 fontSize -= 1;
                 context.font = fontSize+"pt FuturaLight";
-                console.log("text width: " + context.measureText(text).width + " 0.9 image width: " + newWidth);
             }
 
             context.fillText(text, newWidth/2, newHeight/2);
@@ -48,6 +52,12 @@ function readURL(){
 
             //underline text
             underline(context, whatido, newWidth/2 - context.measureText(text).width/2 + context.measureText("I ").width, newHeight/2, 10, "white", 2, 0);
+
+	    //save canvas image as data url (png format by default)
+	    var dataURL = canvas.toDataURL();
+		
+            // set canvasImg image src to dataURL
+            document.getElementById('canvasImg').src = dataURL;
 
         };
         imageObj.src = reader.result;
@@ -60,7 +70,6 @@ function readURL(){
 
 function underline(ctx, text, x, y, fontSize, color, thickness ,offset){
   var width = ctx.measureText(text).width;
-  console.log("text width: "+ width);
 
   y += fontSize+offset;
 
@@ -70,5 +79,4 @@ function underline(ctx, text, x, y, fontSize, color, thickness ,offset){
   ctx.moveTo(x,y);
   ctx.lineTo(x+width,y);
   ctx.stroke();
-
 }
