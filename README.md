01b81ef18ac3414ac9496a2f9772a4d3ace55ace
Francisco Salgado <salgfrancisco@gmail.com>
2016-12-31 17:29:23 +0000

    Added readme file

---
+++ README.md
@@ -0,0 +1,6 @@
+# #mycalvins Generator
+##Create your own #mycalvins ad
+
+Little experiment with javascript and html5 canvas.
+
+![spaz out](spazout.png)
--- script.js
+++ script.js
@@ -1,13 +1,15 @@
+//Canvas dimensions
 var CANVAS_HEIGHT = 480;
 var CANVAS_WIDTH = 640;
 
-
+//Once DOM structure is loaded, add listener to the generate button
 document.addEventListener("DOMContentLoaded", function(event) {
     var generateButton = document.getElementById('generateButton')
     generateButton.addEventListener('click', generate, true);
 });
 
 
+//Loads picture, writes text on it and displays
 function generate(){
     var file = document.getElementById("getFile").files[0];
     var reader = new FileReader();
@@ -19,6 +21,7 @@ function generate(){
 
         var imageObj = new Image();
         imageObj.onload = function(){
+            //Resize picture to canvas
             var ratio = imageObj.width/imageObj.height;
             var newWidth = canvas.width;
             var newHeight = newWidth / ratio;
@@ -26,12 +29,13 @@ function generate(){
                 newHeight = canvas.height;
                 newWidth = newHeight * ratio;
             }
-
             canvas.width = newWidth;
             canvas.height = newHeight;
 
+            //Display picture
             context.drawImage(imageObj, 0, 0, newWidth, newHeight);
 
+            //Write text
             var whatido = document.getElementById('whatido').value;
             var fontSize = 40;
             context.font = fontSize+"pt FuturaLight";
@@ -47,15 +51,15 @@ function generate(){
                 context.font = fontSize+"pt FuturaLight";
             }
 
+            //draw text
             context.fillText(text, newWidth/2, newHeight/2);
-
 
             //underline text
             underline(context, whatido, newWidth/2 - context.measureText(text).width/2 + context.measureText("I ").width, newHeight/2, 10, "white", 2, 0);
 
-	    //save canvas image as data url (png format by default)
-	    var dataURL = canvas.toDataURL();
-		
+	        //save canvas image as data url (png by default)
+	        var dataURL = canvas.toDataURL();
+
             // set canvasImg image src to dataURL
             document.getElementById('canvasImg').src = dataURL;
 
@@ -68,6 +72,7 @@ function generate(){
     }
 }
 
+//underline text
 function underline(ctx, text, x, y, fontSize, color, thickness ,offset){
   var width = ctx.measureText(text).width;
 
